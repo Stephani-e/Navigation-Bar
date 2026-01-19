5ccc100729ee4b2c72e4df6ea9e114504f91ba8e
Stephanie <gad.ephanie@gmail.com>
2026-01-19 22:58:05 +0100

    Update 1.0

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,47 +1,72 @@
-import React, { Component } from 'react'
-import { MenuItems } from './MenuItems'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
-import './NavBar.css'
-import { Button } from '../Button'
+import React, { Component } from "react";
+import { MenuItems } from "./MenuItems";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import "./NavBar.css";
+import { Button } from "../Buttons/Button.jsx";
 
 class NavBar extends Component {
     state = { clicked: false };
+
     handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
+        this.setState((prev) => ({ clicked: !prev.clicked }));
+    };
+
     render() {
-        return(
-            <nav className='NavbarItems'>
-                <div className='navbar-container'>
-                    <h1 className='navbar-logo'>React <FontAwesomeIcon icon={faReact} size='1x'/> </h1>
-                    <div className='menu-icon' onClick={this.handleClick}>
-                        <FontAwesomeIcon icon={this.state.clicked ? faTimes : faBars} className='menu-toggle-icon' />
+        const { clicked } = this.state;
+
+        return (
+            <nav className="Navbar">
+                <div className="Navbar-inner">
+                    <div className="Navbar-brand">
+                        <h1 className="Navbar-logo">
+                            React
+                            <span className="Navbar-logo-icon">
+                <FontAwesomeIcon icon={faReact} />
+              </span>
+                        </h1>
                     </div>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                   {MenuItems.map((item, index) => {
-                      if (item.cName === 'nav-links-mobile'){
-                        return(
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>{item.title}</a>
-                            </li>
-                        );
-                        }else{
-                            return(
-                                <li key={index}>
-                                    <a className={item.cName} href={item.url}>{item.title}</a>
+
+                    <div className="Navbar-toggle" onClick={this.handleClick}>
+                        <FontAwesomeIcon
+                            icon={clicked ? faTimes : faBars}
+                            className="Navbar-toggle-icon"
+                        />
+                    </div>
+
+                    <ul className={clicked ? "Navbar-menu Navbar-menu--active" : "Navbar-menu"}>
+                        {MenuItems.map((item, index) => {
+                            if (item.cName === "nav-links-mobile") {
+                                return (
+                                    <li key={index} className="Navbar-item Navbar-item--mobile">
+                                        <a className={item.cName} href={item.url}>
+                                            {item.title}
+                                        </a>
+                                    </li>
+                                );
+                            }
+
+                            return (
+                                <li key={index} className="Navbar-item">
+                                    <a className={item.cName} href={item.url}>
+                                        {item.title}
+                                    </a>
                                 </li>
                             );
-                        }
-                    })}
-                    {/** Desktop Button */} 
-                    <Button buttonStyle="btn--outline" buttonSize="btn--medium">Sign Up</Button>
-                </ul>
+                        })}
+
+                        {/* Desktop CTA */}
+                        <li className="Navbar-cta">
+                            <Button buttonStyle="btn--primary" buttonSize="btn--medium">
+                                Sign Up
+                            </Button>
+                        </li>
+                    </ul>
+                </div>
             </nav>
-        )
+        );
     }
 }
 
-export default NavBar
+export default NavBar;
